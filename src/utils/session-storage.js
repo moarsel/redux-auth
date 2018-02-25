e819b19b641fb730cd820d7c285271e0cf48bd75
Russell <russell.hoy@gmail.com>
2018-02-25 15:01:02 -0500

    test

--- src/utils/session-storage.js
+++ src/utils/session-storage.js
@@ -1,63 +1,59 @@
 import Cookies from "browser-cookies";
 import * as C from "./constants";
 
-
 // even though this code shouldn't be used server-side, node will throw
 // errors if "window" is used
 var root = Function("return this")() || (42, eval)("this");
 
 // stateful variables that persist throughout session
 root.authState = {
-  currentSettings:    {},
-  currentEndpoint:    {},
+  currentSettings: {},
+  currentEndpoint: {},
   defaultEndpointKey: null
-}
+};
 
-export function setCurrentSettings (s) {
+export function setCurrentSettings(s) {
   root.authState.currentSettings = s;
 }
 
-export function getCurrentSettings () {
+export function getCurrentSettings() {
   return root.authState.currentSettings;
 }
 
-export function setCurrentEndpoint (e) {
+export function setCurrentEndpoint(e) {
   root.authState.currentEndpoint = e;
 }
 
-export function getCurrentEndpoint () {
+export function getCurrentEndpoint() {
   return root.authState.currentEndpoint;
 }
 
-export function setCurrentEndpointKey (k) {
+export function setCurrentEndpointKey(k) {
   persistData(C.SAVED_CONFIG_KEY, k || getDefaultEndpointKey());
 }
 
-export function getCurrentEndpointKey () {
+export function getCurrentEndpointKey() {
   return retrieveData(C.SAVED_CONFIG_KEY) || getDefaultEndpointKey();
 }
 
-export function setDefaultEndpointKey (k) {
+export function setDefaultEndpointKey(k) {
   persistData(C.DEFAULT_CONFIG_KEY, k);
 }
 
-export function getDefaultEndpointKey () {
+export function getDefaultEndpointKey() {
   return retrieveData(C.DEFAULT_CONFIG_KEY);
 }
 
 // reset stateful variables
-export function resetConfig () {
+export function resetConfig() {
   root.authState = root.authState || {};
-  root.authState.currentSettings    = {};
-  root.authState.currentEndpoint    = {};
+  root.authState.currentSettings = {};
+  root.authState.currentEndpoint = {};
   destroySession();
 }
 
-export function destroySession () {
-  var sessionKeys = [
-    C.SAVED_CREDS_KEY,
-    C.SAVED_CONFIG_KEY
-  ];
+export function destroySession() {
+  var sessionKeys = [C.SAVED_CREDS_KEY, C.SAVED_CONFIG_KEY];
 
   for (var key in sessionKeys) {
     key = sessionKeys[key];
@@ -74,20 +70,19 @@ export function destroySession () {
   }
 }
 
-function unescapeQuotes (val) {
+function unescapeQuotes(val) {
   return val && val.replace(/("|')/g, "");
-};
+}
 
-
-export function getInitialEndpointKey () {
+export function getInitialEndpointKey() {
   return unescapeQuotes(
     Cookies.get(C.SAVED_CONFIG_KEY) ||
-    (root.localStorage && root.localStorage.getItem(C.SAVED_CONFIG_KEY))
+      (root.localStorage && root.localStorage.getItem(C.SAVED_CONFIG_KEY))
   );
 }
 
 // TODO: make this really work
-export function getSessionEndpointKey (k) {
+export function getSessionEndpointKey(k) {
   let key = k || getCurrentEndpointKey();
   if (!key) {
     throw "You must configure redux-auth before use.";
@@ -96,47 +91,65 @@ export function getSessionEndpointKey (k) {
   }
 }
 
-export function getSessionEndpoint (k) {
+export function getSessionEndpoint(k) {
   return getCurrentEndpoint()[getSessionEndpointKey(k)];
 }
 
 // only should work for current session
-export function getDestroyAccountUrl (endpointKey) {
-  return `${getApiUrl(endpointKey)}${getSessionEndpoint(endpointKey).accountDeletePath}`
+export function getDestroyAccountUrl(endpointKey) {
+  return `${getApiUrl(endpointKey)}${
+    getSessionEndpoint(endpointKey).accountDeletePath
+  }`;
 }
 
 // only should work for current session
-export function getSignOutUrl (endpointKey) {
-  return `${getApiUrl(endpointKey)}${getSessionEndpoint(endpointKey).signOutPath}`
+export function getSignOutUrl(endpointKey) {
+  return `${getApiUrl(endpointKey)}${
+    getSessionEndpoint(endpointKey).signOutPath
+  }`;
 }
 
-export function getEmailSignInUrl (endpointKey) {
-  return `${getApiUrl(endpointKey)}${getSessionEndpoint(endpointKey).emailSignInPath}`
+export function getEmailSignInUrl(endpointKey) {
+  return `${getApiUrl(endpointKey)}${
+    getSessionEndpoint(endpointKey).emailSignInPath
+  }`;
 }
 
-export function getEmailSignUpUrl (endpointKey) {
-  return `${getApiUrl(endpointKey)}${getSessionEndpoint(endpointKey).emailRegistrationPath}?config_name=${endpointKey}`
+export function getEmailSignUpUrl(endpointKey) {
+  return `${getApiUrl(endpointKey)}${
+    getSessionEndpoint(endpointKey).emailRegistrationPath
+  }?config_name=${endpointKey}`;
 }
 
-export function getPasswordResetRequestUrl (endpointKey) {
-  return `${getApiUrl(endpointKey)}${getSessionEndpoint(endpointKey).passwordResetPath}?config_name=${endpointKey}`
+export function getPasswordResetRequestUrl(endpointKey) {
+  return `${getApiUrl(endpointKey)}${
+    getSessionEndpoint(endpointKey).passwordResetPath
+  }?config_name=${endpointKey}`;
 }
 
-export function getPasswordUpdateUrl (endpointKey) {
-  return `${getApiUrl(endpointKey)}${getSessionEndpoint(endpointKey).passwordUpdatePath}`
+export function getPasswordUpdateUrl(endpointKey) {
+  return `${getApiUrl(endpointKey)}${
+    getSessionEndpoint(endpointKey).passwordUpdatePath
+  }`;
 }
 
-export function getTokenValidationPath (endpointKey) {
-  return `${getApiUrl(endpointKey)}${getSessionEndpoint(endpointKey).tokenValidationPath}`
+export function getTokenValidationPath(endpointKey) {
+  return `${getApiUrl(endpointKey)}${
+    getSessionEndpoint(endpointKey).tokenValidationPath
+  }`;
 }
 
-export function getOAuthUrl ({provider, params, endpointKey}) {
-  var oAuthUrl = getApiUrl(endpointKey) + getSessionEndpoint(endpointKey).authProviderPaths[provider] +
-    "?auth_origin_url="+encodeURIComponent(root.location.href) +
-    "&config_name="+encodeURIComponent(getSessionEndpointKey(endpointKey));
+export function getOAuthUrl({ provider, params, endpointKey }) {
+  var oAuthUrl =
+    getApiUrl(endpointKey) +
+    getSessionEndpoint(endpointKey).authProviderPaths[provider] +
+    "?auth_origin_url=" +
+    encodeURIComponent(root.location.href) +
+    "&config_name=" +
+    encodeURIComponent(getSessionEndpointKey(endpointKey));
 
   if (params) {
-    for(var key in params) {
+    for (var key in params) {
       oAuthUrl += "&";
       oAuthUrl += encodeURIComponent(key);
       oAuthUrl += "=";
@@ -147,11 +160,11 @@ export function getOAuthUrl ({provider, params, endpointKey}) {
   return oAuthUrl;
 }
 
-export function getConfirmationSuccessUrl () {
+export function getConfirmationSuccessUrl() {
   return root.authState.currentSettings.confirmationSuccessUrl();
 }
 
-export function getPasswordResetRedirectUrl () {
+export function getPasswordResetRedirectUrl() {
   return root.authState.currentSettings.confirmationSuccessUrl();
 }
 
@@ -165,8 +178,7 @@ export function getTokenFormat() {
 }
 
 export function removeData(key) {
-
-  switch(root.authState.currentSettings.storage) {
+  switch (root.authState.currentSettings.storage) {
     case "localStorage":
       root.localStorage.removeItem(key);
       break;
@@ -175,8 +187,10 @@ export function removeData(key) {
   }
 }
 
-export function persistData (key, val) {
+export function persistData(key, val) {
   val = JSON.stringify(val);
+  console.log(val);
+  debugger;
 
   switch (root.authState.currentSettings.storage) {
     case "localStorage":
@@ -186,14 +200,13 @@ export function persistData (key, val) {
     default:
       Cookies.set(key, val, {
         expires: root.authState.currentSettings.cookieExpiry,
-        path:    root.authState.currentSettings.cookiePath
+        path: root.authState.currentSettings.cookiePath
       });
       break;
   }
-};
+}
 
-
-export function retrieveData (key, storage) {
+export function retrieveData(key, storage) {
   var val = null;
 
   switch (storage || root.authState.currentSettings.storage) {
@@ -215,4 +228,4 @@ export function retrieveData (key, storage) {
     // unescape quotes
     return unescapeQuotes(val);
   }
-};
+}
